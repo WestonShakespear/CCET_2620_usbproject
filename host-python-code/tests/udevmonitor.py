e4f40b78bebf50fe86fbb282757ad1bdeeae021c
Weston Shakespear <westonshakespear@gmail.com>
2021-12-03 18:43:55 -0700

    udev monitoring works

---
+++ host-python-code/tests/udevmonitor.py
@@ -0,0 +1,8 @@
+import pyudev
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem='usb')
+
+for device in iter(monitor.poll, None):
+    print(device)
